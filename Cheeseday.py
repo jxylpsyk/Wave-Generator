49e494a63263debf12ac17910ddbad52792e8f86
jxylpsyk <83065510+jxylpsyk@users.noreply.github.com>
2021-11-29 15:42:57 +0530

    First fileig
    Cheeeseday it is a cheeeseday

---
+++ Cheeseday.py
@@ -0,0 +1,12 @@
+'''
+Hello so today is a cheeseday
+'''
+import random
+a = random.int(0,9)
+if a < 3:
+  print('Chedar is the way to go')
+elif a>=3 and a<8:
+  print('Mozerella and pizza is good')
+else:
+  print('Why cheese when u can have salt')
+  
