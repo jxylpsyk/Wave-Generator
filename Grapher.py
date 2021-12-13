69d359df97eba8d21edceb68b90d2d2327414113
anirband1 <deshmukh.anirban@gmail.com>
2021-12-13 12:13:42 +0530

    Grapher update

--- Grapher.py
+++ Grapher.py
@@ -1,21 +1,19 @@
-import matplotlib
-
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.style.use('seaborn-dark')
+
+# this should be changed to selected wave array
 user_arr = np.linspace(0, 2, 44100 * 2)
+
 freq = 4000
-graph_cut = 44100 / freq
+single_osc = 44100 / freq
 
 # converts a float value to the integer above it
-if not graph_cut.is_integer():
-    graph_cut = round(graph_cut + 0.5)
+if not single_osc.is_integer():
+    single_osc = round(single_osc + 0.5)
 
-user_arr = user_arr[:int(graph_cut)]
-user_arr = np.sin(user_arr * freq * 2 * np.pi)
+user_arr = user_arr[:int(single_osc)]
+# user_arr = np.sin(user_arr * freq * 2 * np.pi)
 
-plt.style.use('seaborn-dark')
 plt.plot(user_arr)
-
-x = 129.0
-print(x.is_integer())
