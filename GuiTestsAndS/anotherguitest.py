029c5055b98041ac3b1786d5d87281d111fe9c0e
jxylpsyk <emailto.tw5@gmail.com>
2021-12-15 20:21:03 +0530

    ramdas

--- GuiTestsAndS/anotherguitest.py
+++ GuiTestsAndS/anotherguitest.py
@@ -1,11 +1,36 @@
 import tkinter as tk
-
+from PIL import Image,ImageTk,ImageDraw
+'''
+colours pinkish red #ff8cad
+mellow yellow #ffff8c
+Pretty purple #c800ff
+Bratty blue #2600ff
+Plain Blanc #ffffff
+!!!ALWAYS KEEP MIN ROW AS 1
+'''
 def a1():
     print ('hello world')
 
 root = tk.Tk()
-canvas = tk.Canvas(root,height=900,width=1200,background= "#faaffa",insertborderwidth=10, highlightthickness=27,highlightcolor="#fda10f",highlightbackground="#fcabbf")
-canvas.grid(columnspan=15,rowspan=15)
-txtbx1 = tk.Button(root,width=10,height=4)
-txtbx1.grid(column = 3,row = 3)
+root.title("Wave Generator 4.0.0.734")
+canvas = tk.Canvas(root,height=900,width=300,background= "#ff8cad")#,insertborderwidth=10, highlightthickness=27,highlightcolor="#fda10f",highlightbackground="#fcabbf")
+canvas.grid(columnspan=15,rowspan=100)
+box = tk.Canvas(root,height = 900, width = 1200, background = "#ffff8c")
+box.grid(column= 15,row=1,columnspan=100,rowspan=100)
+
+tsxt1 = tk.Label(text = "Wave Generator",height= 0,background="#ff8cad",)
+tsxt1.grid(row=1)
+
+txtbx1 = tk.Button(root,width=12,height=2,text="Square",background = '#2600ff',foreground="#ffffff",)
+txtbx1.grid(column = 1,row = 32)
+
+txtbx1 = tk.Button(root,width=12,height=2,text="SawTooth",background='#2600ff',foreground="#ffffff",)
+txtbx1.grid(column = 9,row = 32)
+
+txtbx1 = tk.Button(root,width=12,height=2,text="Triangle",background='#c800ff',foreground="#ffffff",)
+txtbx1.grid(column = 1,row = 35)
+
+txtbx1 = tk.Button(root,width=12,height=2,text="Sine",background= '#c800ff',foreground="#ffffff",)
+txtbx1.grid(column = 9,row =35)
+
 root.mainloop()
