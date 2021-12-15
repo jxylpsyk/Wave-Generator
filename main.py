df3db4cee5fa63e31575e757fa902505e0eabf82
anirband1 <deshmukh.anirban@gmail.com>
2021-12-15 08:18:18 +0530

    Added list of notes and corresponding frequencies

--- main.py
+++ main.py
@@ -16,13 +16,33 @@ https://simpleaudio.readthedocs.io/en/latest/tutorial.html#playing-audio-directl
 
 SAMPLE_RATE = 44100
 
+notes = {
+    '5C': 440 * 2**(3 / 12),
+    '5Csh': 440 * 2**(4 / 12),
+    '5Db': 440 * 2**(4 / 12),
+    '5D': 440 * 2**(5 / 12),
+    '5Dsh': 440 * 2**(6 / 12),
+    '5Eb': 440 * 2**(6 / 12),
+    '5E': 440 * 2**(7 / 12),
+    '5F': 440 * 2**(8 / 12),
+    '5Fsh': 440 * 2**(9 / 12),
+    '5Gb': 440 * 2**(9 / 12),
+    '5G': 440 * 2**(10 / 12),
+    '5Gsh': 440 * 2**(11 / 12),
+    '5Ab': 440 * 2**(11 / 12),
+    '5A': 440 * 2,
+    '5Ash': 440 * 2**(13 / 12),
+    '5Bb': 440 * 2**(13 / 12),
+    '5B': 440 * 2**(14 / 12),
+}
+
 
 # freq should be in hertz
-def make_audio(freq, time):
+def make_audio(freq, time) -> np.ndarray:
 
     # time is note duration in seconds
     # Returns time * SAMPLE_RATE evenly spaced samples, calculated over the interval [0, T].
-    t = np.linspace(0, time, SAMPLE_RATE * time, False)
+    t = np.linspace(0, time, int(SAMPLE_RATE * time), False)
 
     # generate sine wave notes
     # range(-1, 1)
@@ -38,7 +58,14 @@ def make_audio(freq, time):
     return audio
 
 
-def play_chord(time, *args):
+def play_note(freq, time) -> None:
+    note = make_audio(freq, time)
+
+    play_obj = sa.play_buffer(note, 1, 2, SAMPLE_RATE)
+    play_obj.wait_done()
+
+
+def play_chord(time, *args) -> None:
     note_list = [make_audio(freq, time) for freq in args]
 
     for note in note_list:
@@ -47,16 +74,29 @@ def play_chord(time, *args):
     play_obj.wait_done()
 
 
-A_freq = 440
-A_freq_oct = 440 * 2
-Csh_freq = A_freq * 2**(4 / 12)
-E_freq = A_freq * 2**(7 / 12)
+def play_song():
+    play_note(notes['5Bb'] / 2, 0.1)
+    play_note(notes['5C'], 0.1)
+    play_note(notes['5Csh'], 0.1)
+    play_note(notes['5Bb'] / 2, 0.1)
+    play_note(notes['5F'], 0.4)
+    play_note(notes['5F'], 0.4)
+    play_note(notes['5Eb'], 0.5)
 
-# play_chord(1, A_freq, A_freq_oct, Csh_freq, E_freq)
+    play_note(notes['5Ab'] / 2, 0.1)
+    play_note(notes['5Bb'] / 2, 0.1)
+    play_note(notes['5C'], 0.1)
+    play_note(notes['5Ab'] / 2, 0.1)
+    play_note(notes['5Eb'], 0.4)
+    play_note(notes['5Eb'], 0.4)
+    play_note(notes['5Csh'], 0.5)
 
-note = make_audio(2280, 1)
-play_ything = sa.play_buffer(note, 1, 2, SAMPLE_RATE)
-play_ything.wait_done()
+
+# play_chord(1, notes['5A'], notes['5A']/2, notes['5Csh'] / 2, notes['5E'] / 2)
+
+# play_note(440 * 2**(-1 / 12), 1)
+
+play_song()
 
 # concatenate notes
 # plays it one after the other
