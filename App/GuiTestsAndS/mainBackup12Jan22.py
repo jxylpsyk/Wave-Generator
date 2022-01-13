8e17f89e74217b33acca33581d262905aa3dd03b
jxylpsyk <emailto.tw5@gmail.com>
2022-01-13 12:42:19 +0530

    Backed Up main.py in mainBackup12Jan22.py

--- App/GuiTestsAndS/mainBackup12Jan22.py
+++ App/GuiTestsAndS/mainBackup12Jan22.py
@@ -1,22 +1,35 @@
 import tkinter as tk
-from PIL import Image, ImageTk, ImageDraw
-import numpy as np
+from PIL import Image, ImageTk  #, ImageDraw
 
 from App.applib.Core.wave_class import Wave
-from App.applib.Core.constants import SAMPLE_RATE
+from App.applib.Core.constants import *
 from App.applib.Core.default_waves import make_default_wave
 from App.applib.Core.audio import play_audio
 
 # The main premise of the project is for the user to do manipulations on two sound waves
 # Therefore, there are two waves for the user to play around with
 
+# DETECT OS is done in constants.py
+
 # The two waves are set to A440 sine waves by default
 # TODO: on startup, make the waves initialize to the last saved wave
+# TODO: Update windows UI
 
 user_wave1 = Wave(make_default_wave('sin', 440, 1))
 user_wave2 = Wave(make_default_wave('sin', 440, 1))
 
+focus_wave = user_wave1
+
 #GUI
+root = tk.Tk()
+root.title("Wave Generator 0.6.9.420")
+left_box = tk.Canvas(
+    root, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, background="#ff8cad"
+)  # ,insertborderwidth=10, highlightthickness=27,highlightcolor="#fda10f",highlightbackground="#fcabbf")
+left_box.grid(columnspan=WINDOW_WIDTH, rowspan=WINDOW_HEIGHT)
+
+# canvas = tk.Canvas(root, height=900, width=1200, background="#ffff8c")
+# canvas.grid(column=15, row=1, columnspan=100, rowspan=100)
 '''
 colours pinkish red #ff8cad
 mellow yellow #ffff8c
@@ -27,104 +40,143 @@ Plain Blanc #ffffff
 !!!ALWAYS KEEP MIN ROW AS 1
 '''
 
+
 class image:
-    def importImage(self,relPth,x_codds,y_codds):
+    def importImage(relPth, x_codds, y_codds):
         graph = Image.open(relPth)
         graph = ImageTk.PhotoImage(graph)
-        graph_label = tk.label(image = graph)
+        graph_label = tk.label(image=graph)
         graph_label.image = graph
-        graph_label.grid(column = x_codds,row = y_codds)
+        graph_label.grid(column=x_codds, row=y_codds)
 
-#class sliders:
-    #to be continued (P.S spelling error)
 
-class textBoxs:
-    def __init__(self,pos_x,pos_y):
-        #karma
-        self.pos_x= pos_x
-        self.pos_y= pos_y
+# class sliders:
+# to be continued (P.S spelling error)
+
+
+class textBox:
+    def __init__(self, pos_x, pos_y, wdth):
+        # karma
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.clspn = wdth
         self.textbox = tk.Text(
-                    #root,
-                    width = 12,
-                    height = 2,
-                    background= '#000000',
-                    foreground='#FFFFFF',
-                    )  
-        
-    def display_textbox(self):
-        self.textbox.grid(column=self.pos_x,row=self.pos_y)
+            root,
+            width=wdth,
+            height=2,
+            background='#000000',
+            foreground='#FFFFFF',
+        )
+
+        self.textbox.place(x=self.pos_x, y=self.pos_y)
+
+    def return_text(self):
+        return self.textbox.get(1.0, "end-1c")
 
 
 class blueWaveButton:
+    # style = ttk.style()
+    # style.configure('BW.TButton', foreground = '#ffffff', background = '#2610ff')
     def __init__(self, wave_name, position_x, position_y, freq):
         self.position_x = position_x
         self.position_y = position_y
 
         self.button = tk.Button(
-            #root,
-            width=12,
+            root,
+            width=BUTTON_WIDTH,
             height=2,
             text=wave_name.capitalize(),
             highlightbackground='#2610ff',
             background='#2610ff',
             foreground="#ffffff",
             command=lambda: play_audio(make_default_wave(wave_name, freq, 1)))
-    def display_button(self):
-        self.button.grid(column=self.position_x, row=self.position_y)
+
+        self.button.place(x=self.position_x, y=self.position_y)
+
 
 class blueButton:
-    def __init__(self, name, position_x, position_y):
+    # style = ttk.style()
+    # style.configure('BW.TButton', foreground = '#ffffff', background = '#2610ff')
+    def __init__(self, name, position_x, position_y, lambda_func):
         self.position_x = position_x
         self.position_y = position_y
 
-        self.button = tk.Button(
-            #root,
-            width=12,
-            height=2,
-            text=name.capitalize(),
-            highlightbackground='#2610ff',
-            background='#2610ff',
-            foreground="#ffffff")
-            #command=lambda: play_audio(make_default_wave(wave_name, freq, 1))
+        self.button = tk.Button(root,
+                                width=BUTTON_WIDTH,
+                                height=2,
+                                text=name.capitalize(),
+                                highlightbackground='#2610ff',
+                                background='#2610ff',
+                                foreground="#ffffff",
+                                command=lambda_func)
 
-    def display_button(self):
-        self.button.grid(column=self.position_x, row=self.position_y)
+        self.button.place(x=self.position_x, y=self.position_y)
 
 
-root = tk.Tk()
-root.title("Wave Generator 0.6.9.420")
+if SYSTEM_OS == "Darwin":
+    '''!!!OsX!!!'''
 
-left_box = tk.Canvas(
-    root, height=900, width=300, background="#ff8cad"
-)  #,insertborderwidth=10, highlightthickness=27,highlightcolor="#fda10f",highlightbackground="#fcabbf")
-left_box.grid(columnspan=15, rowspan=100)
+    txtinp = textBox(WINDOW_WIDTH - 580, 130, 50)
 
-canvas = tk.Canvas(root, height=900, width=1200, background="#ffff8c")
-canvas.grid(column=15, row=1, columnspan=100, rowspan=100)
+    saveBx1 = blueButton(
+        'Save', WINDOW_WIDTH - 160, 130,
+        lambda: focus_wave.save_audio(txtinp.return_text())
+        if txtinp.return_text() != '' else 0)
 
-tsxt1 = tk.Label(
-    text="Wave Generatorrrrrr",
-    height=0,
-    background="#ff8cad",
-)
-tsxt1.grid(row=1)
+    txtbx1 = blueWaveButton('sawtooth', WINDOW_WIDTH - 580, 50, 200)
 
+    txtbx2 = blueWaveButton('square', WINDOW_WIDTH - 440, 50, 200)
 
-txtinp = textBoxs(108,10)
+    txtbx3 = blueWaveButton('sin', WINDOW_WIDTH - 300, 50, 200)
 
-txtbx1 = blueWaveButton('sawtooth', 106, 2, 200)
-txtbx1.display_button()
+    txtbx4 = blueWaveButton('triangle', WINDOW_WIDTH - 160, 50, 200)
 
-txtbx2 = blueWaveButton('square', 110, 2, 200)
-txtbx2.display_button()
+elif SYSTEM_OS == "Linux":
+    '''!!!LINUX!!!'''
 
-txtbx3 = blueWaveButton('sin', 104, 2, 200)
-txtbx3.display_button()
+    txtinp = textBox(WINDOW_WIDTH - 580, 350, 50)
 
-txtbx4 = blueWaveButton('triangle', 108, 2, 200)
-txtbx4.display_button()
+    saveBx1 = blueButton('Save', WINDOW_WIDTH - 160, 350,
+                         lambda: print(txtinp.return_text()))
 
-saveBx1 = blueButton('Save', 108, 5)
+    txtbx1 = blueWaveButton('sawtooth', WINDOW_WIDTH - 580, 50, 200)
+
+    txtbx2 = blueWaveButton('square', WINDOW_WIDTH - 440, 50, 200)
+
+    txtbx3 = blueWaveButton('sin', WINDOW_WIDTH - 300, 50, 200)
+
+    txtbx4 = blueWaveButton('triangle', WINDOW_WIDTH - 160, 50, 200)
+
+elif SYSTEM_OS == "Windows":
+    txtinp = textBox(WINDOW_WIDTH - 650, 143, 49)
+
+    saveBx1 = blueButton('Save', WINDOW_WIDTH - 200, 140,
+                         lambda: focus_wave.save_audio(txtinp.return_text())
+                         if txtinp.return_text() != '' else 0)
+
+    txtbx1 = blueWaveButton('sawtooth', WINDOW_WIDTH - 650, 50, 200)
+
+    txtbx2 = blueWaveButton('square', WINDOW_WIDTH - 500, 50, 200)
+
+    txtbx3 = blueWaveButton('sin', WINDOW_WIDTH - 350, 50, 200)
+
+    txtbx4 = blueWaveButton('triangle', WINDOW_WIDTH - 200, 50, 200)
+
+else:
+    print('Please use a valid Opperating system [Windows,MacOS,Linux]')
+
+root.mainloop()
+
+# region Testing GUI ig
+
+# tsxt1 = tk.Label(
+#     text="Wave Generatorrrrrr",
+#     height=0,
+#     background="#ff8cad",
+# )
+# txt1s.grid(row=1)
+
+# =============================================================================
 
 # v2 = tk.DoubleVar()
 
@@ -149,5 +201,4 @@ saveBx1 = blueButton('Save', 108, 5)
 # l4.pack()
 # b2.pack()
 # l2.pack()
-
-root.mainloop()
+# endregion
