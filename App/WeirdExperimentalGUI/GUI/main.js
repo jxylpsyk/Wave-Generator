ee71193d4eb64c0dc3a4331c5e4ef74741b98f8c
JXYLPSYK <emailto.tw5@gmail.com>
2022-01-21 12:26:08 +0530

    deleted comments

--- App/WeirdExperimentalGUI/GUI/main.js
+++ App/WeirdExperimentalGUI/GUI/main.js
@@ -1,1 +1,15 @@
 // Converting tkinter gui into js gui for more modern experience and better cross platform compatability 
+
+console.log("Hello WOrld")
+
+function Carbocation(a){
+    console.timeLog()
+    console.log(a)
+
+}
+
+Carbocation()
+
+while (1>0){
+    console.log("Hello world")
+}
